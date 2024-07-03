1a793fb1e4831825d58a1d3655b2c6528b51f544
Luna Futures <luna.futures+github@gmail.com>
2024-07-03 18:27:50 -0500

    Use package.json's version in build.sh

--- express/build.sh
+++ express/build.sh
@@ -4,7 +4,7 @@ set -ex
 source .env
 webpack
 
-version="1.1"
+version=$npm_package_version
 docker buildx build -t "hue-express:arm-${version}" --platform linux/arm64 --output=type=docker .
 docker tag "hue-express:arm-${version}" "${REGISTRY_URL}/hue-express:arm-${version}"
 docker push "${REGISTRY_URL}/hue-express:arm-${version}"
