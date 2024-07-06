6357ced4f3da461a89516916707e470b7e3aa4f9
Luna Futures <luna.futures+github@gmail.com>
2024-07-06 01:30:34 -0500

    Update build scripts

--- express/build.sh
+++ express/build.sh
@@ -1,10 +1,20 @@
 #! /bin/bash
 set -ex
 
+version=$npm_package_version
+if [ -z $version ]; then
+	echo "\$version cannot be empty. Run this script from \`npm run\`."
+	exit 1
+else
+	echo VERSION=${VERSION}
+fi
+
 source .env
 webpack
 
-version=$npm_package_version
 docker buildx build -t "hue-express:arm-${version}" --platform linux/arm64 --output=type=docker .
 docker tag "hue-express:arm-${version}" "${REGISTRY_URL}/hue-express:arm-${version}"
 docker push "${REGISTRY_URL}/hue-express:arm-${version}"
+
+docker tag "hue-express:arm-${version}" "${REGISTRY_URL}/hue-express:arm-latest"
+docker push "${REGISTRY_URL}/hue-express:arm-latest"
---
+++ rust/build.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+# Read the version from Cargo.toml
+VERSION=$(grep '^version' Cargo.toml | sed -E 's/version = "(.*)"/\1/')
+if [ -z ${VERSION} ]; then
+	echo "\${VERSION} cannot be empty."
+	exit 1
+else
+	echo VERSION=${VERSION}
+fi
+
+
+function debug() (
+	set -ex
+	docker-compose --build up
+)
+
+function buildx() (
+	set -ex
+	docker buildx build -t rust-hue:arm-${VERSION} --platform linux/arm64 --output=type=docker .
+)
+
+function publish() (
+	set -ex
+	docker tag rust-hue:arm-${VERSION} registry.blahblahblah.xyz/rust-hue:arm-${VERSION}
+	docker push registry.blahblahblah.xyz/rust-hue:arm-${VERSION}
+
+	docker tag rust-hue:arm-${VERSION} registry.blahblahblah.xyz/rust-hue:arm-latest
+	docker push registry.blahblahblah.xyz/rust-hue:arm-latest
+)
+
+function all() (
+	cargo build
+	buildx
+	publish
+)
+
+$*
