1ab2067282f0aa435cc5531612a6627fe715a802
Luna Futures <luna.futures+github@gmail.com>
2024-07-10 16:09:33 -0500

    Add more build.sh helpers to install/upgrade/uninstall the helm chart

--- hue-chart/build.sh
+++ hue-chart/build.sh
@@ -1,7 +1,28 @@
 set -ex
 
 function template() (
-	helm template . -f values.yaml -f values.private.yaml > helm-output.yaml
+	helm template . --debug -f values.yaml -f values.private.yaml > helm-output.yaml
+)
+
+RELEASE_NAME=davis
+function install() (
+	helm install ${RELEASE_NAME} . --debug -f values.yaml -f values.private.yaml
+)
+
+function upgrade() (
+	helm upgrade ${RELEASE_NAME} . --debug -f values.yaml -f values.private.yaml
+)
+
+function uninstall() (
+	helm uninstall ${RELEASE_NAME}
+)
+
+function uninstall-dry-run() (
+	helm uninstall ${RELEASE_NAME} --dry-run
+)
+
+function show-installed() (
+	helm list --all --all-namespaces
 )
 
 if [ -z $@ ]; then
