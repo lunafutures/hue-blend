adbb070e8e2c3037a87ced9c54307dc017d5ba46
Luna Futures <luna.futures+github@gmail.com>
2024-07-10 20:03:36 -0500

    Add cert-manager as chart dep

--- hue-chart/build.sh
+++ hue-chart/build.sh
@@ -13,7 +13,7 @@ function lint() (
 )
 
 function create-namespaces() {
-	for namespace in registry; do
+	for namespace in cert-manager registry; do
 		kubectl create namespace ${namespace} --dry-run=client -o yaml | kubectl apply -f -
 	done
 }
@@ -38,6 +38,16 @@ function uninstall-dry-run() (
 
 function show-installed() (
 	helm list --all --all-namespaces
+)
+
+function purge-cert-manager() (
+	# Run after uninstalling the chart
+
+	# Check for anything remaining
+	kubectl api-resources --verbs=list --namespaced -o name | xargs -n 1 kubectl get --show-kind --ignore-not-found -n cert-manager
+
+	# Uninstall CRDs
+	kubectl delete -f https://github.com/cert-manager/cert-manager/releases/download/v1.14.7/cert-manager.crds.yaml
 )
 
 if [ -z "$@" ]; then
